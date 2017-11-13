213dbc6e2da9465a3c0009a31c09bf4e0599ad63
louisdio <louisdio2016@gmail.com>
2017-11-13 10:25:36 +0800

    自动生成所有pojo,mapper,xml

--- src/main/java/com/how2java/tmall/mapper/CategoryMapper.java
+++ src/main/java/com/how2java/tmall/mapper/CategoryMapper.java
@@ -1,16 +1,21 @@
 package com.how2java.tmall.mapper;
 
 import com.how2java.tmall.pojo.Category;
-import com.how2java.tmall.util.Page;
-
+import com.how2java.tmall.pojo.CategoryExample;
 import java.util.List;
 
 public interface CategoryMapper {
-    List<Category> list();
-    //List<Category> list(Page page);
-    //int total();
-    void add(Category category);
-    void delete(int id);
-    Category get(int id);
-    void update(Category category);
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Category record);
+
+    int insertSelective(Category record);
+
+    List<Category> selectByExample(CategoryExample example);
+
+    Category selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Category record);
+
+    int updateByPrimaryKey(Category record);
 }
--- src/main/java/com/how2java/tmall/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/how2java/tmall/service/impl/CategoryServiceImpl.java
@@ -2,8 +2,8 @@ package com.how2java.tmall.service.impl;
 
 import com.how2java.tmall.mapper.CategoryMapper;
 import com.how2java.tmall.pojo.Category;
+import com.how2java.tmall.pojo.CategoryExample;
 import com.how2java.tmall.service.CategoryService;
-import com.how2java.tmall.util.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +16,10 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public List<Category> list() {
-        return categoryMapper.list();
+        CategoryExample example = new CategoryExample();
+        //指定按照id降序排列
+        example.setOrderByClause("id desc");
+        return categoryMapper.selectByExample(example);
     }
 
     //    @Override
@@ -32,21 +35,22 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public void add(Category category) {
-        categoryMapper.add(category);
+        categoryMapper.insertSelective(category);
     }
 
     @Override
     public void delete(int id) {
-        categoryMapper.delete(id);
+        categoryMapper.deleteByPrimaryKey(id);
     }
 
     @Override
     public Category get(int id) {
-        return categoryMapper.get(id);
+
+        return categoryMapper.selectByPrimaryKey(id);
     }
 
     @Override
     public void update(Category category) {
-        categoryMapper.update(category);
+        categoryMapper.updateByPrimaryKeySelective(category);
     }
 }
